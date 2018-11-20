6f3b91c4c60a9ed3a6e7431f5b1bf03635ffe294
Dan <dan.avraham23@gmail.com>
2018-11-20 13:40:27 +0200

    Initial commit

---
+++ src/IDs.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IDs 
+{
+	public static List<String> getIDs()
+	{
+		List<String> ids = new ArrayList<String>();
+		
+		String id1 = "200878627";
+		String id2 = "204736961";
+		//String id3 = "123456789";
+		
+		ids.add(id1);
+		ids.add(id2);
+		//ids.add(id3);
+		
+		return ids;
+	}
+}
