f77c5ea23bf3441964b206ea8371c0b7b1c99a93
Dan <dan.avraham23@gmail.com>
2018-11-26 21:26:46 +0200

    Task 1-3 tasks 1-3 works, task 4 is too slow.

--- src/TilePuzzleHeuristic.java
+++ src/TilePuzzleHeuristic.java
@@ -1,14 +1,22 @@
 
-public class TilePuzzleHeuristic implements IHeuristic
-{
+public class TilePuzzleHeuristic implements IHeuristic {
 
-	@Override
-	public double getHeuristic
-	(
-		IProblemState problemState
-	) 
-	{
-		return 0;
-	}
-	
+    @Override
+    public double getHeuristic(IProblemState problemState) {
+        double ans = 0;
+        if (problemState instanceof TilePuzzleState) {
+            int h, optCol, optRow;
+            int[][] tile = ((TilePuzzleState) problemState)._tilePuzzle;
+            for (int i = 0; i < tile.length; i++) {
+                for (int j = 0; j < tile[i].length; j++) {
+                    optCol = (tile[i][j] + 2) % tile[i].length;
+                    optRow = (tile[i][j] - 1) % tile.length;
+                    h = (Math.abs(j - optCol) + Math.abs(i - optRow)) * tile[i][j];
+                    ans += h;
+                }
+            }
+        }
+        return ans;
+    }
+
 }
